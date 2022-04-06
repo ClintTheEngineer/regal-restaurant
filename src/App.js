6f61f06e862316302ddd2c3cdc98610a1fb39575
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2022-04-06 14:07:00 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { AboutUs, Chef, Location, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <Location />
+    <Footer />
+  </div>
+);
+
+export default App;
